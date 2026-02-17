da4509341f8610e8b9e14b366b5fb395496b02ca
Sonu Lodhi <sp95759201@gmail.com>
2026-02-17 09:47:00 +0530

    add slider animation

---
+++ javaScript/script.js
@@ -0,0 +1,14 @@
+// let slider = document.querySelector(".slider");
+
+// let i = 0;
+
+// function animate() {
+//     i++;
+//     slider.style.transform = `perspective(1400px) rotateX(-15deg) rotateY(${i}deg)`;
+    
+//     if (i < 360) {
+//         requestAnimationFrame(animate);
+//     }
+// }
+
+// animate();
